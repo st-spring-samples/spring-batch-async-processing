635295b94bf9599a4ee53e3c23eb9395556ba501
Sudhir Tumati <mailofsudhir@gmail.com>
2019-10-17 23:18:32 +0800

    feat: upgrade spring boot version

--- src/test/java/com/sudhirt/practice/batch/async/AsyncBatchDemoApplicationTests.java
+++ src/test/java/com/sudhirt/practice/batch/async/AsyncBatchDemoApplicationTests.java
@@ -1,8 +1,7 @@
 package com.sudhirt.practice.batch.async;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.test.JobLauncherTestUtils;
@@ -10,9 +9,7 @@ import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @SpringBatchTest
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/create.sql")
